619cf8b1e0722c1e58dc5791b1987a02cfcc1d35
Daniel Inge <daninge98@gmail.com>
2018-10-11 22:25:13 -0400

    division implemented

--- ad.py
+++ ad.py
@@ -14,6 +14,18 @@ class Node(object):
     def __radd__(self, other):
         return self.__add__(self, other)
 
+    def __sub__(self, other):
+        try:
+            return Subtraction(self, other)
+        except AttributeError:
+            return Subtraction(self, Const(other))
+
+    def __rsub__(self, other):
+        try:
+            return Subtraction(other, self)
+        except AttributeError:
+            return Subtraction(Const(other), self)
+
     def __mul__(self, other):
         try:
             return Multiplication(self, other)
@@ -22,6 +34,45 @@ class Node(object):
 
     def __rmul__(self, other):
         return self.__mul__(self, other)
+
+    def __truediv__(self, other):
+        try:
+            return Division(self, other)
+        except AttributeError:
+            return Division(self, Const(other))
+
+    def __rtruediv__(self, other):
+        try:
+            return Division(other, self)
+        except AttributeError:
+            return Division(Const(other), self)
+        
+
+class Addition(Node):
+    def __init__(self, a, b):
+        a.is_node()
+        b.is_node()
+        self.a = a
+        self.b = b
+
+    def eval(self, vars={}):
+        return self.a.eval(vars) + self.b.eval(vars)
+
+    def diff(self, vars={}):
+        return self.a.diff(vars) + self.b.diff(vars)
+
+class Subtraction(Node):
+    def __init__(self, a, b):
+        a.is_node()
+        b.is_node()
+        self.a = a
+        self.b = b
+
+    def eval(self, vars={}):
+        return self.a.eval(vars) - self.b.eval(vars)
+
+    def diff(self, vars={}):
+        return self.a.diff(vars) - self.b.diff(vars)
 
 class Multiplication(Node):
     def __init__(self, a, b):
@@ -36,18 +87,18 @@ class Multiplication(Node):
     def diff(self, vars={}):
         return self.a.eval(vars) * self.b.diff(vars) + self.a.diff(vars) * self.b.eval(vars)
 
-class Addition(Node):
+class Division(Node):
     def __init__(self, a, b):
-        a.is_node()
-        b.is_node()
         self.a = a
         self.b = b
+        a.is_node()
+        b.is_node()
 
     def eval(self, vars={}):
-        return self.a.eval(vars) + self.b.eval(vars)
+        return self.a.eval(vars) / self.b.eval(vars)
 
     def diff(self, vars={}):
-        return self.a.diff(vars) + self.b.diff(vars)
+        return (self.a.diff(vars) * self.b.eval(vars) - self.a.eval(vars) * self.b.diff(vars)) / (self.b.eval(vars) * self.b.eval(vars))
 
 class Const(Node):
     def __init__(self, a):
@@ -65,6 +116,6 @@ class Var(Node):
     
     def eval(self, vars={}):
         return vars[self.var_name]
-        
+
     def diff(self, vars={}):
         return 1
