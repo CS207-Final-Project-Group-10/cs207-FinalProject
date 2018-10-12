35660fcf52883baf6094153e9055f5622ccd492d
Daniel Inge <daninge98@gmail.com>
2018-10-11 22:48:44 -0400

    Tidied up, added limited tests

--- ad.py
+++ ad.py
@@ -12,7 +12,10 @@ class Node(object):
             return Addition(self, Const(other))
 
     def __radd__(self, other):
-        return self.__add__(self, other)
+        try:
+            return Addition(self, other)
+        except AttributeError:
+            return Addition(self, Const(other))
 
     def __sub__(self, other):
         try:
@@ -33,7 +36,10 @@ class Node(object):
             return Multiplication(self, Const(other))
 
     def __rmul__(self, other):
-        return self.__mul__(self, other)
+        try:
+            return Multiplication(self, other)
+        except AttributeError:
+            return Multiplication(self, Const(other))
 
     def __truediv__(self, other):
         try:
@@ -99,3 +105,27 @@ class Var(Unop):
 
     def diff(self, vars={}):
         return 1
+
+if __name__ == "__main__":
+    # Examples
+    # f(x) = 5x
+    f_x = 5 * Var('x')
+    assert(f_x.eval({'x':2}) == 10)
+    assert(f_x.diff({'x':2}) == 5)
+
+    # f(x) = 1 + (x * x)
+    f_x = 1 + Var('x') * Var('x')
+    assert(f_x.eval({'x':2}) == 5)
+    assert(f_x.diff({'x':3}) == 6)  
+
+    # f(x) = (1 + x)/(x * x) 
+    f_x = (1 + Var('x')) / (Var('x') * Var('x'))
+    assert(f_x.eval({'x':2}) == 0.75)
+    assert(f_x.diff({'x':2}) == -0.5)
+
+    # f(x) = (1 + 5x)/(x * x) 
+    f_x = (1 + 5 * Var('x')) / (Var('x') * Var('x'))
+    assert(f_x.eval({'x':2}) == 2.75)
+    assert(f_x.diff({'x':2}) == -1.5)
+
+    print("Tests passed")
