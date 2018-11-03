17733b6a75c42a6af412fe1a1a50f91cd05c1c5e
njeinstein <njeinstein@gmail.com>
2018-11-02 21:15:37 -0400

    Added module reference to test_basics.py

--- fluxions/tests/test_basics.py
+++ fluxions/tests/test_basics.py
@@ -1,24 +1,24 @@
 import pytest
-import fluxions
+import fluxions as fl
 
 def test_basic_usage():
     # f(x) = 5x
-    f_x = 5 * Var('x')
+    f_x = 5 * fl.Var('x')
     assert(f_x.eval({'x':2}) == 10)
     assert(f_x.diff({'x':2}) == 5)
 
     # f(x) = 1 + (x * x)
-    f_x = 1 + Var('x') * Var('x')
+    f_x = 1 + fl.Var('x') * fl.Var('x')
     assert(f_x.eval({'x':2}) == 5)
     assert(f_x.diff({'x':3}) == 6)  
 
     # f(x) = (1 + x)/(x * x) 
-    f_x = (1 + Var('x')) / (Var('x') * Var('x'))
+    f_x = (1 + fl.Var('x')) / (fl.Var('x') * fl.Var('x'))
     assert(f_x.eval({'x':2}) == 0.75)
     assert(f_x.diff({'x':2}) == -0.5)
 
     # f(x) = (1 + 5x)/(x * x) 
-    f_x = (1 + 5 * Var('x')) / (Var('x') * Var('x'))
+    f_x = (1 + 5 * fl.Var('x')) / (fl.Var('x') * fl.Var('x'))
     assert(f_x.eval({'x':2}) == 2.75)
     assert(f_x.diff({'x':2}) == -1.5)
 
