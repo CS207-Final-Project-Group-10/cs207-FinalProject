42e2f7ddb36791d3f67c8822271955ee05278f47
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2018-11-04 09:27:53 -0500

    Removed redundant copies of the same tests

--- fluxions/test/test_basics.py
+++ fluxions/test/test_basics.py
@@ -10,27 +10,6 @@ from fluxions import fluxions as fl
 
 # *************************************************************************************************
 def test_basic_usage():
-    # f(x) = 5x
-    f_x = 5 * fl.Var('x')
-    assert(f_x.val({'x':2}) == 10)
-    assert(f_x.diff({'x':2}) == 5)
-    assert(repr(f_x)=="Multiplication(Var(x, None), Const(5))")
-
-    # f(x) = 1 + (x * x)
-    f_x = 1 + fl.Var('x') * fl.Var('x')
-    assert(f_x.val({'x':2}) == 5)
-    assert(f_x.diff({'x':3}) == 6)
-
-    # f(x) = (1 + x)/(x * x)
-    f_x = (1 + fl.Var('x')) / (fl.Var('x') * fl.Var('x'))
-    assert(f_x.val({'x':2}) == 0.75)
-    assert(f_x.diff({'x':2}) == -0.5)
-
-    # f(x) = (1 + 5x)/(x * x)
-    f_x = (1 + 5 * fl.Var('x')) / (fl.Var('x') * fl.Var('x'))
-    assert(f_x.val({'x':2}) == 2.75)
-    assert(f_x.diff({'x':2}) == -1.5)
-
     # Create a variable, x
     x = fl.Var('x', 1.0)
 
