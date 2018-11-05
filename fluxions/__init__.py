16a5f75642b8ff9e18d75406f76399328ec160a4
Daniel Inge <daninge98@gmail.com>
2018-11-04 21:45:04 -0500

    Updated License and __init__.py for pypi

--- fluxions/__init__.py
+++ fluxions/__init__.py
@@ -1,6 +1,9 @@
 # Modules in this package
 __all__ = ['fluxion_node', 'elementary_functions']
 
+# PyPI Name
+name = "fluxions"
+
 # The "public API" of the fluxions package
 from .fluxion_node import Fluxion, Var, Unop, Const, Binop
 from .fluxion_node import Addition, Subtraction, Multiplication, Division, Power
