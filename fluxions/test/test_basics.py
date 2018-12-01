b3c790e32de815b5bf3c79935d0adbd384730717
Steven Burke <Steven@Stevens-MacBook-Air-5.local>
2018-12-01 13:55:08 -0500

    cleaning up tests

--- fluxions/test/test_basics.py
+++ fluxions/test/test_basics.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import os
 # Handle import of module fluxions differently if module
-# module is being loaded as __main__ or a module in a package.
+# is being loaded as __main__ or a module in a package.
 if __name__ == '__main__':
     cwd = os.getcwd()
     os.chdir('../..')
@@ -16,13 +16,13 @@ def report_success():
     """Report that a test was successful"""
     test_name = sys._getframe(1).f_code.co_name
     print(f'{test_name:25}: **** PASS ****')
-    
+
 
 def test_basic_usage():
     """Test basic usage of Fluxions objects"""
     # Create a variable, x
     x = fl.Var('x', 1.0)
-    
+
     #f0 = x - 1
     f0 = x - 1
     # assert(f0.val({'x':1}) == 0)
@@ -33,7 +33,7 @@ def test_basic_usage():
     assert val == 0
     assert diff == 1
     assert repr(f0) == "Subtraction(Var(x, 1.0), Const(1.0))"
-    
+
     # f1(x) = 5x
     f1 = 5 * x
     f1.set_var_names('x')
@@ -46,27 +46,27 @@ def test_basic_usage():
     assert(f1.diff({'x':2}) == 5)
     assert(f1({'x':2}) == (10.0, np.array([5.])))
     assert repr(f1) == "Multiplication(Var(x, 1.0), Const(5.0))"
-    
+
     # f2(x) = 1 + (x * x)
     f2 = 1 + x * x
     f2.set_var_names('x')
     assert(f2(4.0) == (17.0, 8.0))
     assert(f2.val({'x':2}) == 5)
     assert(f2.diff({'x':3}) == 6)
-    
+
     # f3(x) = (1 + x)/(x * x)
     f3 = (1 + x) / (x * x)
     f3.set_var_names('x')
     assert(f3.val({'x':2}) == 0.75)
     assert(f3.diff({'x':2}) == -0.5)
     assert repr(f3) == "Division(Addition(Var(x, 1.0), Const(1.0)), Multiplication(Var(x, 1.0), Var(x, 1.0)))"
-    
+
     # f4(x) = (1 + 5x)/(x * x)
     f4 = (1 + 5 * x) / (x * x)
     f4.set_var_names('x')
     assert(f4.val({'x':2}) == 2.75)
     assert(f4.diff({'x':2}) == -1.5)
-    
+
     # Take a power
     f5 = fl.Power(x, 2)
     f5.set_var_names('x')
@@ -76,7 +76,7 @@ def test_basic_usage():
     assert(f5(1) == (1.0, 2.0))
     assert(f5({'x':1}) == (1.0, 2.0))
     assert repr(f5) == "Power(x, 2)"
-    
+
     #check assignment
     a = fl.Fluxion()
     assert a.is_node()
@@ -90,27 +90,26 @@ def test_basic_usage():
     assert(c.diff({'x':1,'y':1},{'x':2,'y':1}) == np.array([[2]])).all()
     c.set_val(0)
     assert(c(1)==(1, np.array([1])))
-    
+
     #check division
     f6 = 1/x
     assert(f6.val({'x':1,'y':1}) == 1)
     assert(f6.diff({'x':1,'y':1}) == np.array([[-1.]])).all()
-    
+
     #check subtraction and division
     f7 = (1 - x + 1 - 1) / ((x * x)/1)
     f7.set_var_names('x')
     assert(f7.val({'x':2}) == -0.25)
     assert(f7.diff({'x':2}) == 0)
-    
+
     # check negation
     f8 = -x
     assert(f8.val({'x':2}) == -2)
     assert(f8.diff({'x':2}) == -1)
-    
+
     y = fl.Var('y')
     f9 = -(x * y)
     assert(f9.val({'x':-2, 'y':3}) == 6)
-    #assert(f9.val({'x':-2, 'y':3}, {'x':1}))
 
 # Report results
 report_success()
@@ -122,36 +121,35 @@ def test_basics_vectors():
     n = 10
     xs = np.linspace(0,1,num=n)
     ys = np.linspace(1,2,num=n)
-    
+
     # Create a variable, x bound to vector value
     x = fl.Var('x', xs)
-    
+
     # f1(x) = 5x
     f1 = 5 * x
     f1.set_var_names('x')
     assert(f1.val(xs) == 5*xs).all()
     assert(f1.diff({'x':xs}) == 5.0*np.ones(np.shape(xs))).all()
-    
+
     # f2(x) = 1 + (x * x)
     f2 = 1 + x * x
     f2.set_var_names('x')
     assert(f2.val({'x':xs}) == 1 + np.power(xs,2)).all()
     assert(f2.diff({'x':xs}) == 2.0*xs).all()
-    
+
     # f3(y) = (1 + y)/(y * y)
     y = fl.Var('y', ys)
     f3 = (1 + y) / (y * y)
     f3.set_var_names('y')
     assert(f3.val({'y':ys}) == np.divide(1+ys,np.power(ys,2))).all()
     assert np.isclose(f3.diff({'y':ys}), np.divide(-2-ys,np.multiply(np.power(ys,2),ys))).all()
-    
-    
+
     # f(x) = (1 + 5x)/(x * x)
     f4 = (1 + 5*x) / (x * x)
     f4.set_var_names('x')
     assert(f4.val({'x':ys}) == np.divide(1+5*ys,np.power(ys,2))).all()
     assert np.isclose(f4.diff({'x':ys}),np.divide(-2-5*ys,np.multiply(np.power(ys,2),ys))).all()
-    
+
     # f5(x,y) = 5x+y
     f5 = 5 * x + y
     f5.set_var_names(['x', 'y'])
@@ -162,8 +160,7 @@ def test_basics_vectors():
     assert(f5.diff(var_tbl_scalar, {'y': 1}) == np.array([1])).all()
     assert(f5.val(var_tbl_vector) == 5*xs + ys).all()
     assert(f5.diff(var_tbl_vector, {'x':1}) == np.asarray([np.array([5])]*n)).all()
-    
-    # f(x,y) = 5xy
+
     # f(x,y) = 5xy
     f6 = 5 * x * y
     assert(f6.val(var_tbl_scalar) == 30)
@@ -192,7 +189,7 @@ def test_basics_vectors():
     assert(f7.diff(var_tbl_vector, {'x':1}) == np.asarray([np.array([3])]*50)).all()
     assert(f7.diff(var_tbl_vector, {'y':1}) == np.asarray([np.array([2])]*50)).all()
     assert(f7.diff(var_tbl_vector, {'z':1}) == np.asarray([np.array([1])]*50)).all()
-    
+
     # f(x,y,z) = (3x+2y+z)/xyz
     f8 = (x * 3 + 2 * y + z)/(x * y * z)
     f8.set_var_names(['x', 'y', 'z'])
@@ -204,9 +201,9 @@ def test_basics_vectors():
     # Rebind 'x', 'y', ans 'z' to the values in ys (slightly tricky!)
     var_tbl_vector = {'x':ys,'y':ys,'z':ys}
     assert(f8.val(var_tbl_vector) == (3*ys + 2*ys + ys)/(ys*ys*ys)).all()
-    # assert np.isclose(f8.diff(var_tbl_vector), 
+    # assert np.isclose(f8.diff(var_tbl_vector),
     #                  np.transpose([-3*ys/np.power(ys,4), -4*ys/np.power(ys,4), -5*ys/np.power(ys,4)])).all()
-    
+
     #f(x,y) = xy
     f9 = y*x
     assert(f9.val({'x':0,'y':0,'z':1})==0)
--- fluxions/test/test_elementary_functions.py
+++ fluxions/test/test_elementary_functions.py
@@ -22,7 +22,7 @@ def test_elementary_functions():
     # Create a variable theta with angles from 0 to 360 degrees, with values in radians
     theta_val = np.linspace(0, 2*np.pi, 361)
     theta = fl.Var('theta')
-    
+
     # Scalar version
     f_theta = fl.sin(theta)
     assert f_theta.val({'theta':2}) == np.sin(2)
@@ -41,32 +41,32 @@ def test_elementary_functions():
 # ***************************************************************************************
 def test_basics_singlevar():
     theta_vec = np.linspace(-5,5,21) * np.pi
-    
+
     #### TEST: passing in vector of values: "immediate" evaluation
     _cos, _dcos = fl.cos(theta_vec).forward_mode()
     _sin, _dsin = fl.sin(theta_vec).forward_mode()
     _tan, _dtan = fl.tan(theta_vec).forward_mode()
-    
+
     assert(all(_dcos == -_sin))
     assert(all(np.isclose(_tan, _sin/_cos)))
-    
-    
+
+
     #### TEST: passing a fluxion Var: "delayed" evaluation
-    
+
     theta = fl.Var('theta')
     _cos = fl.cos(theta)
     _sin = fl.sin(theta)
-    
+
     assert(np.all(np.isclose(_sin.diff({'theta':theta_vec}), np.cos(theta_vec))))
     assert(np.all(_cos.diff({'theta':theta_vec}) == -1*_sin.val({'theta':theta_vec})))
-    
-    
+
+
     #### TEST: basic functionality of other elementary functions
     # tan' = sec^2
     _dtan = (fl.tan(theta)).diff({'theta':theta_vec})
     _sec2 = ((1/fl.cos(theta))**2).val({'theta':theta_vec})
     assert(np.all(np.isclose(_dtan, _sec2)))
-    
+
     # test Fluxions returns NaN as numpy does
     x = np.linspace(-5,5,21)
     _varx = fl.Var('x')
@@ -83,7 +83,7 @@ def test_basics_singlevar():
         _log_der_1 = _log.diff({'x':x})
         _log_der_2 = 1/_varx.val({'x':x})
         assert(np.all((_log_der_1 == _log_der_2)))
-    
+
     _hypot, _hypot_der = fl.hypot(fl.sin(theta_vec).val(), fl.cos(theta_vec).val()).forward_mode()
     assert(np.all(_hypot == np.ones_like(theta_vec)))
 
@@ -99,48 +99,48 @@ def test_compositions():
             (ii) compositions of elementary functions & other ops (Fluxions)
     """
     theta_vec = np.linspace(-5,5,21) * np.pi
-    
-    # composition of 2 elementary functions: 
+
+    # composition of 2 elementary functions:
     # (a) immediate evaluation
     val, diff = fl.log(fl.exp(theta_vec)).forward_mode()
     assert(np.all(val == theta_vec ))
     assert(np.all(np.isclose(diff, 1.0)))
-    
+
     # (b) delayed evaluation
     logexp = fl.log(fl.exp(fl.Var('theta')))
     assert(np.all(logexp.val({'theta':theta_vec}) == theta_vec ))
     assert(np.all(np.isclose(logexp.diff({'theta':theta_vec}), np.ones_like(theta_vec))))
-    
+
     # composition of elementary functions and basic ops (other Fluxions)
     # (a) immediate evaluation
     ans_1 = fl.cos(theta_vec).val()**2 - fl.sin(theta_vec).val()**2
     ans_2 = fl.cos(2*theta_vec).val()
     assert(np.all(np.isclose(ans_1, ans_2)))
-    
+
     # (b) delayed evaluation
-    theta = fl.Var('theta', theta_vec)    
+    theta = fl.Var('theta', theta_vec)
     f = fl.cos(theta)**2 - fl.sin(theta)**2
     assert(np.all(np.isclose(f.val({'theta':theta_vec}), fl.cos(2*theta_vec).val())))
-    
-    
+
+
     report_success()
 
 
 # ***************************************************************************************
 def test_basics_multivar():
-    """ TEST: elementary functions of multiple variables """ 
+    """ TEST: elementary functions of multiple variables """
     theta_vec = np.linspace(-5,5,21) * np.pi
-    
+
     sin_z = fl.sin( fl.Var('x') * fl.Var('y') )
     assert(np.all(np.isclose(sin_z.val({'x':theta_vec, 'y':theta_vec}), np.sin(theta_vec**2))))
-    
+
     #WHAT TO DO WHEN TOO FEW VARS PASSED? 
     # sin_z.val({'x':theta_vec})
-    
+
     # HOW ARE PARTIALS EVALUATED?
     # sin_z.diff({'x': theta_vec})
     # sin_z.diff({'x': theta_vec, 'y': theta_vec*2})
-    
+
     report_success()
 
 
