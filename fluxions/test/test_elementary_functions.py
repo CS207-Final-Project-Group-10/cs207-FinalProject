ef9451194b7b3f6e1093d7e40e0c034d1ea22812
Steven Burke <Steven@Stevens-MacBook-Air-5.local>
2018-12-03 23:36:39 -0500

    finishing up tests of elementary functions

--- fluxions/test/test_elementary_functions.py
+++ fluxions/test/test_elementary_functions.py
@@ -27,6 +27,7 @@ def test_elementary_functions():
     f_theta = fl.sin(theta)
     assert f_theta.val({'theta':2}) == np.sin(2)
     assert f_theta.diff({'theta':2}) == np.cos(2)
+    assert(str(f_theta)=="sin(Var(theta, None))")
 
     # Vector version
     f_theta = fl.sin(theta)
@@ -149,6 +150,8 @@ def test_trig():
     theta = np.expand_dims(np.linspace(-5,5,21), axis=1)
 
     # Evaluation and derivative of sin(x), cos(x) and tan(x)
+    assert(str(fl.sin)=="DifferentiableFunctionFactory(sin)")
+    assert(str(fl.sin(0))=="FluxionResult(0.0,1.0)")
     _sin, _dsin = fl.sin(theta)()
     _cos, _dcos = fl.cos(theta)()
     _tan, _dtan = fl.tan(theta)()
@@ -170,7 +173,7 @@ def test_trig():
 # ***************************************************************************************
 def test_inverse_trig():
     """Test the inverse trigonometric functions"""
-    theta = np.expand_dims(np.linspace(-0.9,0.9,21)*np.pi/2, axis=1)
+    theta = np.expand_dims(np.linspace(-0.9,0.9,21), axis=1)
 
     # Evaluate x, y and t at theta
     _y = np.sin(theta)
@@ -179,19 +182,28 @@ def test_inverse_trig():
 
     # Evaluation and derivative of arcsin(x), arccos(x) and arctan(x)
     _arcsin, _darcsin = fl.arcsin(_y)()
-    _arccos, _darccos = fl.arccos(_x)()
+    _arccos, _darccos = fl.arccos(theta)()
     _arctan, _darctan = fl.arctan(_t)()
+    _arcsinh, _darcsinh = fl.arcsinh(theta)()
+    _arccosh, _darccosh = fl.arccosh(theta+2)()
+    _arctanh, _darctanh = fl.arctanh(theta)()
 
     # Ignore divide by zero errors (they are not real errors here)
     with np.errstate(divide='ignore'):
         # Known answers
         # arcsin'(y) = 1 / sqrt(1-y^2)
         assert np.allclose(_darcsin, 1.0 / np.sqrt(1.0 - _y**2))
-        # arccos'(x) = -1 / sqrt(1-x^2)
-        assert np.allclose(_darccos, -1.0 / np.sqrt(1.0 - _x**2))
+        # arccos'(theta) = -1 / sqrt(1-theta^2)
+        assert np.allclose(_darccos, -1.0 / np.sqrt(1.0 - theta**2))
         # arctan'(t) = 1 / (1+t^2)
         assert np.allclose(_darctan, 1.0 / (1.0 + _t**2))
-    
+        # arcsinh'(theta) = 1 / sqrt(1+theta^2)
+        assert np.allclose(_darcsinh, 1.0 / np.sqrt(theta*theta + 1))
+        # arccosh'(theta+2) = 1 / sqrt((theta+2)^2-1)
+        assert np.allclose(_darccosh, 1.0 / np.sqrt((theta+2)**2 - 1))
+        # arctanh'(theta) = 1 / (1-theta^2)
+        assert np.allclose(_darctanh, 1.0 / (1.0 - theta*theta))
+
     report_success()
 
 
@@ -251,7 +263,7 @@ def test_hyperbolic():
     _sinh, _dsinh = fl.sinh(x)()
     _cosh, _dcosh = fl.cosh(x)()
     _tanh, _dtanh = fl.tanh(x)()
-          
+
     # Known answers
     assert np.allclose(_sinh, (exp_x - exp_mx)/2.0)
     assert np.allclose(_dsinh, _cosh)
@@ -268,22 +280,22 @@ def test_exp_log():
     x = np.linspace(-5, 5, 21)    
     exp_x = np.exp(x)
     y = exp_x
-    
+
     # Evaluate exp(x), log(y)
     _exp, _dexp = fl.exp(x)()
     _log, _dlog = fl.log(y)()
-    
+
     # Known answers
     assert np.allclose(_exp, exp_x)
     assert np.allclose(_dexp, exp_x)
     assert np.allclose(_log, x)
     assert np.allclose(_dlog, 1.0 / y)
-    
+
     # Log base 2 and 10; exp base 2
     _log2, _dlog2 = fl.log2(y)()
     _log10, _dlog10 = fl.log10(y)()
     _exp2, _dexp2 = fl.exp2(x)()
-    
+
     # Known answers
     assert np.allclose(_log2, x / np.log(2.0))
     assert np.allclose(_dlog2, 1.0 / y / np.log(2.0))
@@ -291,17 +303,34 @@ def test_exp_log():
     assert np.allclose(_dlog10, 1.0 / y / np.log(10.0))    
     assert np.allclose(_exp2, 2.0**x)
     assert np.allclose(_dexp2, np.log(2.0) * (2.0**x) )
-    
+
     # exponential minus 1, log plus 1
     _expm1, _dexpm1 = fl.expm1(x)()
     _log1p, _dlog1p = fl.log1p(y)()
-        
+
     # Known answers
     assert np.allclose(_expm1, exp_x - 1.0)
     assert np.allclose(_dexpm1, exp_x)
     assert np.allclose(_log1p, np.log(1.0 + y))
     assert np.allclose(_dlog1p, 1.0 / (1.0 + y))
-    
+
+    # exponential minus 1, log plus 1
+    _logaddexp, _dlogaddexp = fl.logaddexp(x,x)()
+    _logaddexp2, _dlogaddexp2 = fl.logaddexp2(x,x)()
+    assert(str(fl.logaddexp(fl.Var('x'),fl.Var('y')))=="logaddexp(Var(x, None),Var(y, None))")
+
+    # Known answers
+    assert np.allclose(_logaddexp, np.logaddexp(x,x))
+    assert np.allclose(_dlogaddexp, np.vstack([0*y + 1/2, 0*y + 1/2]).T)
+    assert np.allclose(_logaddexp2, np.logaddexp2(x,x))
+    assert np.allclose(_dlogaddexp2, np.vstack([0*y + 1/2, 0*y + 1/2]).T)
+
+    # forward mode
+    f = fl.logaddexp(fl.Var('x'),fl.Var('y'))
+    val, diff = f(0,0)
+    assert(np.isclose(val,np.array([[0.69314718]])))
+    assert(diff.all()== np.array([[0.5, 0.5]]).all())
+
     report_success()
 
 
